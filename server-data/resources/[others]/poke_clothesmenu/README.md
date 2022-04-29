d51f554b83c93c7c72336f28de833443c6543426
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-04-29 23:01:43 +0100

    Add files via upload

---
+++ server-data/resources/[others]/poke_clothesmenu/README.md
@@ -0,0 +1,10 @@
+# poke_clothesmenu
+Clothes menu for off/on your clothes!
+
+## 1. Installation
+- Add ```ensure poke_clothesmenu``` in your server.cfg.
+- You can change language on ```config.lua``` file.
+
+## 2. Info
+- KLC
+- To get support --> [Discord](http://discord.vorpcore.com/)
---
+++ server-data/resources/[others]/poke_clothesmenu/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'rdr3'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+client_scripts {
+    'locale.lua',
+    'locales/en.lua',
+    'locales/es.lua',
+    'config.lua',
+    'client/warmenu.lua',
+    'client/main.lua'
+}
