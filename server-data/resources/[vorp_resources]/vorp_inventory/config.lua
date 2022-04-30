c46f8063e8c1d5ba612188e6e1bd43e10675f479
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-04-30 15:49:27 +0100

    Update config.lua

--- server-data/resources/[vorp_resources]/vorp_inventory/config.lua
+++ server-data/resources/[vorp_resources]/vorp_inventory/config.lua
@@ -4,7 +4,7 @@
 
 Config  = {
 
-  Debug = true, -- if your server is live set this to false.  to true only if you are testing things
+  Debug = false, -- if your server is live set this to false.  to true only if you are testing things
   DevMode = false, -- if your server is live set this to false.  to true only if you are testing things (auto load inventory when script restart and before character selection. Alos add /getInv command)
   
   defaultlang = "en_lang",
