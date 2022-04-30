732ea768ff413b4c9bbcf58b304443618a8b464e
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-04-30 15:46:50 +0100

    Add files via upload

---
+++ server-data/resources/[syn2]/syn_bank/config.lua
@@ -0,0 +1,47 @@
+Config = {}
+
+Config.banks = {
+    ["Valentine Bank"] =  {x=-308.50,y=776.24,z=118.75, city = "Valentine", blipsprite = -2128054417},
+    ["Blackwater Bank"] =  {x=-813.18,y=-1277.60,z=43.68, city = "Blackwater", blipsprite = -2128054417},
+    ["Saint Denis Bank"] =  {x=2644.08,y=-1292.21,z=52.29, city = "Saint Denis2", blipsprite = -2128054417},
+    ["Rhodes Bank"] =  {x=1293.1,y=-1301.0,z=77.0, city = "Saint Denis", blipsprite = -2128054417},
+} 
+
+Config.adminwebhook = ""
+Config.webhookavatar = "https://www.pngmart.com/files/5/Bank-PNG-Transparent-Picture.png"
+Config.gold = false
+
+Config.keys = {
+    ["G"] = 0x760A9C6F,
+    ["F"] = 0xB2F377E8,
+	["BACKSPACE"] = 0x156F7119,
+    ["ENTER"] = 0xC7B5340A,
+}
+
+Config.language = {
+    openmenu = "Menu",
+    bank = "Bank",
+    welcome = "Welcome To The Bank",
+    cashbalance = "Money: $",
+    goldbalance = "Gold: ",
+    cashbalance2 = "Your Savings.",
+    takecash = "Withdraw Cash",
+    depocash = "Deposit Cash",
+    takegold = "Withdraw Gold",
+    depogold = "Deposit Gold",
+    takecash2 = "Withdraw Your Cash",
+    depocash2 = "Deposit Your Cash",
+    takegold2 = "Withdraw Your Gold",
+    depogold2 = "Deposit Your Gold",
+    confirm = "Confirm",
+    amount = "Amount",
+    youdepo = "You Deposited $",
+    youdepog = "You Deposited G",
+    invalid = "Invalid Amount",
+    withdrew = "You Withdrew $",
+    withdrewg = "You Withdrew G",
+    withc = "Withdraw Cash",
+    withg = "Withdraw Gold",
+    depoc = "Deposit Cash",
+    depog = "Deposit Gold",
+}
