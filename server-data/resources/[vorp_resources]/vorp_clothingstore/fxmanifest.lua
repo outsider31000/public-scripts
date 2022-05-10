78c4fe1b85e9f9d1b1e5388540dd1b3368175b5e
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-10 19:49:14 +0100

    Update fxmanifest.lua

--- server-data/resources/[vorp_resources]/vorp_clothingstore/fxmanifest.lua
+++ server-data/resources/[vorp_resources]/vorp_clothingstore/fxmanifest.lua
@@ -14,3 +14,9 @@ files {
   'MenuAPI.dll',
   'Newtonsoft.Json.dll'
 }
+
+--dont touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/VORP-ClothingStore'
