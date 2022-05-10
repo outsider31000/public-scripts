6aa816e3432eef92d78c960db126a7fd53ddfeb2
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-10 19:52:09 +0100

    Update fxmanifest.lua

--- server-data/resources/[vorp_resources]/vorp_weaponsv2/fxmanifest.lua
+++ server-data/resources/[vorp_resources]/vorp_weaponsv2/fxmanifest.lua
@@ -3,6 +3,23 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game "rdr3"
 file 'wepcomps.json'
 
-client_script {'client/warmenu.lua', 'client/client.lua'}
-server_script {'server/server.lua'}
-shared_scripts {'config/shops.lua','config/weapons.lua','config/language.lua','config/ammo.lua','config/config.lua'}
+client_script {
+  'client/warmenu.lua',
+  'client/client.lua'
+}
+server_script {
+  'server/server.lua'
+}
+shared_scripts {
+  'config/shops.lua',
+  'config/weapons.lua',
+  'config/language.lua',
+  'config/ammo.lua',
+  'config/config.lua'
+}
+
+--dont touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_weaponsv2'
