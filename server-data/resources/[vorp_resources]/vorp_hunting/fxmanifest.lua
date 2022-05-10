2e643138140ea473593ceb2d8ea256bf3408c390
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-10 20:07:38 +0100

    Update fxmanifest.lua

--- server-data/resources/[vorp_resources]/vorp_hunting/fxmanifest.lua
+++ server-data/resources/[vorp_resources]/vorp_hunting/fxmanifest.lua
@@ -20,3 +20,9 @@ server_script {
 exports {
 	'DataViewNativeGetEventData'
 }
+
+--dont touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/VORP-Hunting'
