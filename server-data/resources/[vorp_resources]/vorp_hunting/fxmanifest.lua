dd939dfe54b21d95e357cc4a6de5ed59b99df2c0
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-14 17:38:40 +0100

    Update fxmanifest.lua

--- server-data/resources/[vorp_resources]/vorp_hunting/fxmanifest.lua
+++ server-data/resources/[vorp_resources]/vorp_hunting/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'VORP Core | rubi216 | Artzalez'
 description 'Hunting script for VORP Core'
-version 'alpha'
+
 
 client_script {
     'config.lua',
