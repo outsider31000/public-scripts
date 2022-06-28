b0a61bc077fe325f33dc22839b62551a32601a2f
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-06-28 12:11:54 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -33,11 +33,15 @@ INSERT INTO `banks` (`name`) VALUES
 
 -- Dumping structure for table vorpv2.bank_users
 CREATE TABLE IF NOT EXISTS `bank_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `identifier` varchar(50) NOT NULL,
   `charidentifier` int(11) NOT NULL,
   `money` double(22,2) DEFAULT 0.00,
   `gold` double(22,2) DEFAULT 0.00,
+  `items` longtext DEFAULT '[]',
+  `invspace` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `name` (`name`),
   CONSTRAINT `bank` FOREIGN KEY (`name`) REFERENCES `banks` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
