78ecdfd852f3fb4f50c8581611dcb93ebf39489c
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-07-02 22:40:03 +0100

    Delete server-data/resources/[vorp_resources]/vorp_core/vorp_core directory

--- server-data/resources/[vorp_resources]/vorp_core/vorp_core/client/cl_uicore.lua
+++
@@ -1,9 +0,0 @@
-RegisterNetEvent('vorp:updateUi', function(stringJson)
-    SendNUIMessage(json.decode(stringJson))
-end)
-
-RegisterNetEvent('vorp:showUi', function(active)
-    local jsonpost = {type="ui",action="hide"}
-    if active then jsonpost = {type="ui",action="show"} end
-    SendNUIMessage(jsonpost)
-end)
