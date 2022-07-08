d7d374e25f83b5e77d3cd948119d798011966049
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-07-08 19:13:21 +0100

    Delete server-data/resources/[vorp_resources]/vorp_core directory

--- server-data/resources/[vorp_resources]/vorp_core/client/cl_playeractions.lua
+++
@@ -1,5 +0,0 @@
-function TeleportToCoords(x, y, z, heading)
-    local playerPedId = PlayerPedId()
-    SetEntityCoords(playerPedId, x, y, z, true, true, true, false)
-    if heading ~= nil then SetEntityHeading(playerPedId, heading) end
-end
