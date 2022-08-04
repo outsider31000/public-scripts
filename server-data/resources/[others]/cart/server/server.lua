94f1dba9f3eeab4388e3098d4a98a59823913535
Hobbs <101003021+DerHobbs@users.noreply.github.com>
2022-08-04 04:24:05 +0200

    Performance fix

--- server-data/resources/[others]/cart/server/server.lua
+++ server-data/resources/[others]/cart/server/server.lua
@@ -1,6 +1,5 @@
 RegisterServerEvent('EveryoneTeleportEntity')
 AddEventHandler('EveryoneTeleportEntity', function(ent,x,y,z) 
 nent = NetworkGetEntityFromNetworkId(ent)
-
 TriggerClientEvent('EveryoneTeleportEntity',-1,ent,x,y,z)
 end)
