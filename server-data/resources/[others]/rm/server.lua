ded2f9bbac697ae9b1df8c3cf38f147ec6678301
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-10-13 21:22:19 +0100

    Update server.lua

--- server-data/resources/[others]/rm/server.lua
+++ server-data/resources/[others]/rm/server.lua
@@ -45,19 +45,3 @@ AddEventHandler("syn_walkanim:setwalk", function(animation)
     exports.ghmattimysql:execute("UPDATE characters Set walk=@walk WHERE identifier=@identifier AND charidentifier = @charidentifier", {['walk'] = walk,['identifier'] = identifier, ['charidentifier'] = charidentifier})
 end)
 
-Citizen.CreateThread(function()
-    Citizen.Wait(500)
-    VorpInv.RegisterUsableItem("secondchance", function(data)
-        VorpInv.subItem(data.source, "secondchance", 1)
-        TriggerClientEvent("syn_walkanim:secondchance2",data.source)
-        VorpInv.CloseInv(data.source)
-    end)
-end)
-
-RegisterServerEvent("syn_walkanim:secondchance")
-AddEventHandler("syn_walkanim:secondchance", function()
-    local _source = source
-    local Character = VorpCore.getUser(_source).getUsedCharacter
-    local charidentifier = Character.charIdentifier
-    TriggerClientEvent("vorpcharacter:secondchance",_source,charidentifier,Character)
-end)
